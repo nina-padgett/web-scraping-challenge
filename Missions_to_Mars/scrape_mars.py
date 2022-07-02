363fdfd33304dcfcc22d1cad73eab7f7fcf16007
Nina Padgett <nina.padgett@northwestern.edu>
2022-07-02 16:57:57 -0500

    update jupyter notebook file and remove old code that was commented

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -8,13 +8,15 @@ import pandas as pd
 import pymongo
 import time
 import datetime as dt
-def scrape_all():
-# Setup splinter
 
+
+def scrape_all():
+    
+# Setup splinter
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
+    
     latest_news_title, latest_news_paragraph = latest_news_title_and_paragraph_function(browser)
-    #stored_img_urls = find_stored_img_urls(browser)
 
     all_scraped_elements_dict = {"latest_news_title": latest_news_title, 
                                 "latest_news_paragraph": latest_news_paragraph, 
@@ -25,14 +27,14 @@ def scrape_all():
     browser.quit
     return all_scraped_elements_dict
    
-# Extract the latest news title
+# Create a function to extract the latest news title
 def latest_news_title_and_paragraph_function(browser):
     # Use browser to visit the URL
     url = 'https://redplanetscience.com/'
     browser.visit(url)
 
-    # Wait 3 seconds
-    time.sleep(3)
+    # Wait 1 seconds
+    time.sleep(1)
 
     # Retrieve page
     html = browser.html
@@ -48,25 +50,22 @@ def latest_news_title_and_paragraph_function(browser):
     return latest_news_title, latest_news_paragraph
 
 
-### JPL Mars Space Images-Featured Image
-# Extract the Mars Space Featured Image
+# Create a function to extract the Mars Space Featured Image
 def mars_featured_img_function(browser):
     # Use browser to visit the URL
     image_url = 'https://spaceimages-mars.com/'
     browser.visit(image_url)
 
-    # Wait 3 seconds
-    # time.sleep(3)
-    # find and click the full image button
     full_img = browser.find_by_tag("button")[1]
     full_img.click()
+
     # Retrieve page
     html = browser.html
 
     # Create Beautiful Soup Object
     image_soup = bs(html, 'html.parser')
-    #type(image_soup)
-# error handling
+   
+    # error handling
     try:
         featured_image_url = image_soup.find('img', class_='fancybox-image').get('src')
     except AttributeError:
@@ -75,45 +74,32 @@ def mars_featured_img_function(browser):
     return image_url
 
 
-
-
-### Mars Facts
+# Create a function to extract Mars Facts
 def mars_facts_function():
     try: 
         facts_url = 'https://galaxyfacts-mars.com/'
         df = pd.read_html(facts_url)[0]
     except BaseException:
         return None
-    
-    #browser.visit(facts_url)
 
-    # Wait 3 seconds
-    time.sleep(3)
+    # Wait 1 second
+    time.sleep(1)
 
     # Read html table
-    
-    #df=tables[0]
     df.columns=['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
-    #df=df.iloc[1:]
-# Clean table by removing unwanted newlines 
+    
+    # Clean table by removing unwanted newlines 
     facts_html = df.to_html(classes='table table-striped')
     return facts_html
     facts_soup = bs(html, '')
 
 
-
-
+# Create a function to extract Mars Hemisphere Images
 def hemispheres(browser):
     all_hemispheres_url = 'https://marshemispheres.com/'
     browser.visit(all_hemispheres_url + 'index.html')
     stored_img_urls = []
-    # for i in range(4):
-    #     browser.find_by_css('img.thumb')[i].click() 
-    #     browser.find_by_text('Sample')['href']
-    #     img_url = browser.find_by_text('Sample')['href']
-    #     stored_img_urls.append(img_url)
-    #     browser.back()
     
     for i in range(4):
 
@@ -121,9 +107,6 @@ def hemispheres(browser):
         hemi_data = scrape_hemisphere(browser.html)
         hemi_data["img_url"] = all_hemispheres_url + hemi_data["img_url"]
         stored_img_urls.append(hemi_data)
-       #  browser.find_by_text('Sample')['href']
-        #  img_url = browser.find_by_text('Sample')['href']
-        #  stored_img_urls.append({"img url": img_url})
         browser.back()
     return stored_img_urls
 
@@ -139,32 +122,6 @@ def scrape_hemisphere(html_text):
     hemispheres = {"title":title_elements, "img_url":sample_elements}
     return hemispheres
 
-
 #scrape_all()
 if __name__ == "__main__":
     print(scrape_all())
-
-    # ### Mars Hemispheres
-    # # Use browser to visit the URL
-    # def find_stored_image_urls():
-    # all_hemispheres_url = 'https://marshemispheres.com/'
-    # browser.visit(all_hemispheres_url)
-
-    # # Create Beautiful Soup Object
-    # soup = bs(browser.html, 'html.parser')
-
-
-    # # Create dictionary to store the full resolution hemisphere images and hemisphere titles
-    # # Create lists of image titles and image URLs to store in the dictionary
-    # hemisphere_image_dict = {}
-    # stored_img_urls = []
-
-    # # Create for loop to scrape the full resolution hemisphere images
-    # for i in range(4):
-    #     browser.find_by_css('img.thumb')[i].click() 
-    #     browser.find_by_text('Sample')['href']
-    #     img_url = browser.find_by_text('Sample')['href']
-    #     stored_img_urls.append({"img url": img_url})
-    #     browser.back()
-    # return stored_img_urls
-    
